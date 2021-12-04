a0202e5e52710405b344ddbf76891737907ddd06
Ducksword <46074810+Ducksword@users.noreply.github.com>
2021-12-03 21:20:26 -0500

    Update beatpack-downloader.ps1

--- beatpack-downloader.ps1
+++ beatpack-downloader.ps1
@@ -1,24 +1,24 @@
-# Url to download maps
-$kitsu = "https://kitsu.moe/d/"
+$Beatconnect = "https://beatconnect.io/b/"
 
 $path = Read-Host -Prompt "Enter path for osu Songs folder"
 [int]$parameter1 = Read-Host -Prompt "Enter pack range start"
 [int]$parameter2 = Read-Host -Prompt "Enter pack range end"
-$ProgressPreference = 'SilentlyContinue'
 
 While ($parameter1 -le $parameter2){
   
     Write-Output "Now Downloading Pack $parameter1 Out Of $parameter2"
 
-    $url = "https://osu.ppy.sh/beatmaps/packs/$parameter1"
-    $responce = (Invoke-WebRequest -Uri $url).Links.href
+    $osuurl = "https://osu.ppy.sh/beatmaps/packs/$parameter1"
+    $responce = (Invoke-WebRequest -Uri $osuurl).Links.href
     $filter = $responce | Select-String -Pattern "https://osu.ppy.sh/beatmapsets/"
     $mapset = $filter -replace "https://osu.ppy.sh/beatmapsets/"
 
     $mapset | ForEach-Object {
-        Invoke-WebRequest -Uri $kitsu+$_ -OutFile $path\$_.osz -UseBasicParsing
-        Start-Sleep -Seconds 2
+        $wc = New-Object System.Net.WebClient
+        $wc.DownloadFile($Beatconnect+$_, "$path\$_.osz")
+        Start-Sleep -Seconds 3
     }
+
     $parameter1 = $parameter1 + 1
 
 }
